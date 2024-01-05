17cbd27652f9c5963ed62b2b229a1e5b6168aedb
Deeraj Balamurugan <70526612+deeraj-balamurugan@users.noreply.github.com>
2024-01-05 22:02:20 +0530

    Add files via upload

---
+++ 7. Quote Generator/main.js
@@ -0,0 +1,24 @@
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const api_url = "https://api.quotable.io/random";
+
+async function getquote(url) {
+  const res = await fetch(url);
+  var data = await res.json();
+  quote.innerHTML = data.content;
+  author.innerHTML = data.author;
+}
+
+getquote(api_url);
+
+function tweet() {
+  window.open(
+    "https://twitter.com/intent/tweet?text=Hello%20world" +
+      quote.innerHTM +
+      "- by " +
+      author.innerHTML,
+    "Tweet Window",
+    "width=600",
+    "height=400"
+  );
+}
